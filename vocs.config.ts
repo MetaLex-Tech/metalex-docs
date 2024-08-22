ae11a10016c98ff972c8731d5375a90d42a1a2a2
merisman <6125373+merisman@users.noreply.github.com>
2024-08-22 17:24:08 -0500

    nav structure update

--- vocs.config.ts
+++ vocs.config.ts
@@ -5,12 +5,49 @@ export default defineConfig({
   sidebar: [
     {
       text: 'MetaLeX OS',
-      link: '/onchain-borgs',
+      collapsed: false,
+      items: [
+        {
+          text: 'Introduction',
+          link: '/os/metalex-os-intro',
+        },
+        {
+          text: 'DAO Landing Page',
+          link: '/os/dao/dao-landing',
+          items: []
+        },
+        {
+          text: 'BORG Landing Page',
+          link: '/os/borg/borg-landing',
+          collapsed: false, 
+          items: [
+            {
+              text: 'BORG Types',
+              link: '/os/borg/borg-types',
+              collapsed: true, 
+              items: [
+                {
+                  text: 'grantsBORG',
+                  link: '/os/borg/borg-types/grantsborg',
+                },
+                {
+                  text: 'devBORG',
+                  link: '/os/borg/borg-types/devborg',
+                },
+                {
+                  text: 'securityBORG',
+                  link: '/os/borg/borg-types/securityborg',
+                }
+              ]
+            }
+          ]
+        }
+      ],
     },
     {
       text: 'Intro to BORGs',
       link: '/intro-to-borgs',
-      collapsed: true, 
+      collapsed: false, 
       items: [ 
         {
           text: 'Onchain BORGs',
